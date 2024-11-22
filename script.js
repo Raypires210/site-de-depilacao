52ead6ecd2cc53126b0f2462dd489c8255f847fd
Karen-silva842 <karensuelen721@gmail.com>
2024-11-22 00:07:08 -0300

    MEXI NO HTML, CSS E JAVA SCRIPT

---
+++ script.js
@@ -0,0 +1,38 @@
+const fotos = ["baner.PNG","buco.PNG"]
+
+const tempointervalo = 2000
+
+var indice = 0, temporizador;
+
+window.onload = () => {
+
+    const moldura = document.getElementById("moldura")
+    const botaoVoltar = document.getElementById("btnVoltar")
+    const botaoAvancar = document.getElementById("btnAvancar")
+
+    const MostrarFoto = () => {
+        moldura.src = "imagem/" + fotos[indice]
+    }
+
+    const automaticoFoto = () => {
+        clearInterval(temporizador)
+        temporizador = setInterval(() => AvancarFoto(), tempointervalo)
+    }
+
+    const AvancarFoto = () => {
+        indice = (indice + 1) % fotos.length
+        MostrarFoto()
+        automaticoFoto()
+    }
+
+    const VoltarFoto = () => {
+        indice = (indice - 1 + fotos.length) % fotos.length
+        MostrarFoto()
+        automaticoFoto()
+    }
+
+    botaoAvancar.onclick = AvancarFoto
+    botaoVoltar.onclick = VoltarFoto
+    MostrarFoto()
+    AvancarFoto()
+}
